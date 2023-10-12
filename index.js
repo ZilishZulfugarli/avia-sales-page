7e781de17d23eab45e47f158e40d0609ae93fa3d
ZilishZulfugarli <2002zilis@gmail.com>
2023-10-12 17:11:19 +0400

    Homepage finished...

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ hamburgerIcon.addEventListener("click", ()=>{
         sideMenu.style.display = "none";
     }
 })
+
+
+let col = document.getElementById("col");
+let destination = document.getElementById("destination-name");
+let show = document.getElementById("show");
+
+col.addEventListener("touchstart", ()=>{
+        destination.style.display = "none"
+        show.style.display = "block"
+})
+
+col.addEventListener("touchend", ()=>{
+        destination.style.display = "block"
+        show.style.display = "none"
+})
